a1b0fe9dc5cd37eefeb0cca758a55e87e7975fb9
Jaromir Rivera <jaromirrivera@gmail.com>
2019-11-21 15:39:43 -0500

    Cleaning up and removing --name for batch transcoding

--- transcode-video.sh
+++ transcode-video.sh
@@ -2,7 +2,7 @@
 
 supported_types = %w(mov flv wmv avi divx mpg mpeg mp4 mkv)
 output_dir = "./output"
-docker_command = "docker run --privileged --device=\"/dev/dri:/dev/dri\" -itv \"`pwd`\":/data --name transcode-video jaromirrivera/transcode-video-quicksync:v1.0"
+docker_command = "docker run --privileged --device=\"/dev/dri:/dev/dri\" -itv \"`pwd`\":/data jaromirrivera/transcode-video-quicksync:v1.0"
 
 glob = "./*\.{#{supported_types.join(',')}}"
 
@@ -34,19 +34,4 @@ Dir.glob(glob).each do |input_path|
     raise "transcoding failed: #{error}"
   end
 end
-
-#transcode_command="transcode-video --target small --abr -o \"/output/$m4v\" --handbrake-option encoder=x265 --audio-width all=surround --ac3-bitrate 384 --pass-ac3-bitrate 384 \"/input/$mkv\""
-
-# docker run --rm \
-#            --cpuset-cpus="1-2" \
-#            --name handbrake \
-#            -v "$(pwd)/$map:/input:ro" \
-#            -v "${CONVERTED_FILE_LOCATION}:/output:rw" \
-#            supercoder/docker-handbrake-cli:latest \
-#            -i "/input/$mkv" \
-#            -o "/output/$m4v" \
-#            -e x265  -q 20.0 -r 30 --pfr  -a 1 -E faac -B 160 -6 dpl2 -R Auto \
-#            -D 0.0 --audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 \
-#            -f mp4 -X 1280 -Y 720 --loose-anamorphic --modulus 2 -m \
-#            --x264-preset medium --h264-profile high --h264-level 3.1
 
